53560fca76d22b62f2d2aa60485389e1f1dbfd64
Jeff <JeffreyRiggle@gmail.com>
2019-06-20 06:33:04 -0400

    simple style touch up

--- generator.js
+++ generator.js
@@ -1,5 +1,16 @@
 const fs = require('fs');
 
+function generateHeader() {
+    return `
+        <header>
+            <h3>
+                <a href>Home</a>
+                <a href>Projects</a>
+                <a href>Blog</a>
+            </h3>
+        </header>
+    `
+}
 function generateLinks() {
     let retVal = '<ul>';
     fs.readdirSync('./build').forEach(file => {
@@ -13,9 +24,26 @@ const generateIndex = () => {
     let content = `
     <html>
         <head>
-            <title>Something</title>
+            <title>My Site</title>
+            <style>
+                body {
+                    margin: 0;
+                }
+                header {
+                    background-color: black;
+                    padding: 12px;
+                }
+                header a {
+                    color: green;
+                    margin-right: 8px;
+                }
+                ul {
+                    list-style: none;
+                }
+            </style>
         </head>
         <body>
+            ${generateHeader()}
             ${generateLinks()}
         </body>
     </html>
